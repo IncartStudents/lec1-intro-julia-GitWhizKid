f0fee774675b7744c261a3c0338a2643382ecc5d
GitWhizKid <forvoxchannel@gmail.com>
2025-03-24 07:27:16 +0300

    Initial stage lvl 4

--- level4_boids.jl
+++ level4_boids.jl
@@ -1,22 +1,28 @@
 module Boids
 using Plots
+using Random
 
 
 mutable struct WorldState
-    boid::Tuple{Float64, Float64}
+    boids::Vector{Tuple{Float64, Float64}}
     height::Float64
     width::Float64
     function WorldState(n_boids, width, height)
-        # TODO: добавить случайные позиции для n_boids птичек вместо одной
-        
-        new((width/2, height/2), width, height)
+        random_boids = [(rand() * width, rand() * height) for _ in 1:n_boids]
+        new(random_boids, width, height)
     end
 end
 
 function update!(state::WorldState)
-    state.boid = state.boid .+ 0.1
-    # TODO: реализация уравнения движения птичек
+    for i in 1:length(state.boids)
+        state.boids[i] = (state.boids[i][1] + 0.1, state.boids[i][2] + 0.1)
 
+        state.boids[i] = (
+            clamp(state.boids[i][1], 0, state.width),
+            clamp(state.boids[i][2], 0, state.height)
+        )
+
+    end
     return nothing
 end
 
@@ -29,8 +35,7 @@ function (@main)(ARGS)
 
     anim = @animate for time = 1:100
         update!(state)
-        boids = [state.boid]
-        scatter(boids, xlim = (0, state.width), ylim = (0, state.height))
+        scatter(state.boids, xlim = (0, state.width), ylim = (0, state.height))
     end
     gif(anim, "boids.gif", fps = 10)
 end
